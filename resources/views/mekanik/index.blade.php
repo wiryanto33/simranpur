ecf7877d59f880ec2291abc892c98ae36f160a46
wiryanto33 <indrarencongsakti33@gmail.com>
2026-05-20 14:24:36 +0700

    update:button double add mekanik

--- resources/views/mekanik/index.blade.php
+++ resources/views/mekanik/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-page-header title="Data Mekanik" subtitle="Kelola data tim mekanik pemeliharaan ranpur">
         <x-slot name="actions">
-            @can('create_mekanik')
+            {{-- @can('create_mekanik')
             <button wire:click="$dispatch('createMekanik')"
                     class="flex items-center px-4 py-2 text-sm font-medium text-white transition-colors border border-transparent rounded-md shadow-sm bg-[#1B3A2D] hover:bg-[#2D5A45] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#C8A84B]">
                 <svg class="w-5 h-5 mr-2 -ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -9,7 +9,7 @@
                 </svg>
                 Tambah Mekanik
             </button>
-            @endcan
+            @endcan --}}
         </x-slot>
     </x-page-header>
 
